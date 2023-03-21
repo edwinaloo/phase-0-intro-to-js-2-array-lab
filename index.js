5bfd4f55a0cd181093a675d45e1958f0080ee695
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-21 12:46:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-  };
-  
-  const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-  };
-  
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  const createFareMultiplier = function(multiplier) {
-    return function(fare) {
-      return fare * multiplier;
-    };
-  };
-  
-  const fareDoubler = createFareMultiplier(2);
-  
-  const fareTripler = createFareMultiplier(3);
-  
-  const selectDifferentDrivers = function(drivers, returnDriversFunction) {
-    return returnDriversFunction(drivers);
-  };
-  
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Add elements to the end of the array destructively
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// Add elements to the beginning of the array destructively
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// Remove the last element from the array destructively
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// Remove the first element from the array destructively
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Return a new array with the added element at the end, without modifying the original array
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// Return a new array with the added element at the beginning, without modifying the original array
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// Return a new array without the last element, without modifying the original array
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+// Return a new array without the first element, without modifying the original array
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
 // Write your solution here!
